bd63101a641caaf5894694ccb07a6f25b25b29f3
Christopher Tso <christso@gmail.com>
2025-11-14 19:17:22 +1100

    remove redundant type files

--- packages/core/src/evaluation/providers/targets-file.d.ts
+++
@@ -1,3 +0,0 @@
-import type { TargetDefinition } from "./types";
-export declare function readTargetDefinitions(filePath: string): Promise<readonly TargetDefinition[]>;
-export declare function listTargetNames(definitions: readonly TargetDefinition[]): readonly string[];
